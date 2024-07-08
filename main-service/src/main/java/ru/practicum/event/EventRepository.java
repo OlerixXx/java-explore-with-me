930a9258a59abdbf5387101b74f87978b8ac6270
OlerixXx <olegzka.mprozov@yandex.ru>
2024-07-08 16:08:47 +0300

    Дипломный проект

--- main-service/src/main/java/ru/practicum/event/EventRepository.java
+++ main-service/src/main/java/ru/practicum/event/EventRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.event.model.Event;
-import ru.practicum.event.model.State;
 
 import java.time.LocalDateTime;
 import java.util.List;
